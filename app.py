9607cfe8f955a481b0f0d847678d9fe8387eb907
Noam Rubin <noamrubin@live.com>
2012-08-24 15:57:55 -0400

    started transitioning to SQLAlchemy. Need to write form submit handlers for login and account creation, that'll help me work with the DB.

--- app.py
+++ app.py
@@ -2,17 +2,29 @@ from flask import Flask, session, render_template, request, redirect, url_for, a
 import datetime
 from Tasky import Task, Tasklist
 import json
-import sqlite3
+from sqlalchemy import *
 
 app = Flask(__name__)
 app.secret_key = "\x8d\xd4\x9b\xef\x8fB\xa2\x02\xd9\x9a\xd5\xd4\x8eD\x1b'\xdf\n\x8b4\x8fhB\xbb"
 tasks = []
 tasklists = {}
-db = sqlite3.connect('TASKY_DB.db')
+db = create_engine('sqlite:///TASKY_DB.db')
+db.echo = False
+metdata = BoundMetaData(db)
+tasks = Table('tasks',metdata,autoload=True)
+tasklists = Table('tasklists',metdata,autoload=True)
+users = Table('users',metdata,autoload=True)
 
 @app.route('/')
+def home():
+    return render_template('login.html')
+
+
+@app.route('/login',methods=['GET','POST'])
 def login():
-    session['tasks'] = {}
+    # if request.method == 'POST':
+    #     #select all of the user's tasklists and tasks from db, populate page with them
+    # session['tasks'] = {}
     session['tasklists'] = {}
     print db
     return render_template('welcome.html',now=str(datetime.datetime.now()),tasks=[])
