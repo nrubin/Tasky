39ed9ae2e3722de31be7e0f20d2ced84469c0606
Noam Rubin <noamrubin@live.com>
2012-08-25 00:07:07 -0400

    ugh flask-login and dbs are fucking with my head! id seems to get called before user validation

--- app.py
+++ app.py
@@ -1,50 +1,53 @@
 from flask import Flask, session, render_template, request, redirect, url_for, abort, jsonify
-import datetime
-from Tasky import Task, Tasklist
+from models import User, Task, Tasklist
 import json
-from sqlalchemy import *
-from sqlalchemy.orm import sessionmaker
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import *
+import uuid
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TASKY_DB.db'
+app.config['SQLALCHEMY_ECHO'] = True
 app.secret_key = "\x8d\xd4\x9b\xef\x8fB\xa2\x02\xd9\x9a\xd5\xd4\x8eD\x1b'\xdf\n\x8b4\x8fhB\xbb"
-tasks = []
-tasklists = {}
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.setup_app(app)
 
-def setup_db(server_address='sqlite:///TASKY_DB.db',echo=True,convert_unicode=True):
-    engine = create_engine(server_address,echo)
-    metadata = MetaData(engine)
-    #loaded necessary tables
-    users = Table('users',metadata,autoload=True)
-    tasks = Table('tasks',metadata,autoload=True)
-    tasklists = Table('tasklists',metadata,autoload=True)
-
-
-
-
-@app.route('/',methods=['GET','POST'])
+@app.route('/')
 def home():
-    if request.method == 'POST':
-        if request.form['email'] != None:
-            #someone is signing up
-            print 'now we add their info to the db'
-            return redirect(url_for('view_tasks'))
-        else:
-            #someone is loggin i
-            print 'now we log them in and make sure they are in the db'
-            return redirect(url_for('view_tasks'))
     return render_template('login.html')
 
-
 @app.route('/login',methods=['GET','POST'])
-def view_tasks():
-    # if request.method == 'POST':
-    #     #select all of the user's tasklists and tasks from db, populate page with them
-    # session['tasks'] = {}
-    return render_template('welcome.html',now=str(datetime.datetime.now()),tasks=[])
+def login():
+    if request.method == 'POST':
+        print 'about to create user object'
+        user = User(request.form['username'],request.form['password'])
+        print 'ive created the object'
+        login_user(user)
+        flash("Logged In successfuly")
+        return redirect(url_for('view_tasks'))
+    return render_template('login.html')
 
-# def create_task_list():
+@app.route('/newaccount',methods=['POST'])
+def create_account():
+    new_user = User(request.form['username'],request.form['password'])
+    if not new_user.exists():
+        db.session.add(new_user)
+        return redirect(url_for('view_tasks'))
+    else:
+        flash("You already exist")
+        return 'fuck you'
+
+
+
+def view_tasks():
+    return render_template('welcome.html',now=str(datetime.utcnow()),tasks=[])
+
+def create_task_list():
+    pass
 
 @app.route('/newtask',methods=['POST'])
+@login_required
 def create_task():
     newTaskContent = request.form['newTaskContent']
     newTask = Task(newTaskContent)
@@ -54,6 +57,135 @@ def create_task():
 @app.route('/updateTasks',methods=['GET'])
 def update_tasks():
     return render_template('task.html',tasks=sorted(tasks, key=lambda task: task.date_created,reverse=True))
+
+
+class Task(db.Model):
+
+    #Initialize what we need for mapping between SQL and an instance object
+    __tablename__ = 'tasks'
+    id = db.Column(db.BigInteger, primary_key=True)
+    date_created = db.Column(db.DateTime)
+    content = db.Column(db.String)
+    deadline = db.Column(db.DateTime)
+    priority = db.Column(db.Integer)
+    completed = db.Column(db.Boolean)
+    archived = db.Column(db.Boolean)
+
+    #relate it to a user and a task list
+    # tasklist_id = db.Column(db.BigInteger, db.ForeignKey('tasklist.id'))
+    # user_id = db.Column(db.BigInteger, db.ForeignKey('user.id'))
+
+
+    #initializes the instance with default values.
+    def __init__(self,content,deadline=None,priority=0):
+        self.content = content
+        self.deadline = deadline
+        self.priority = priority
+        self.completed = False
+        self.date_created = datetime.utcnow()
+        self.id = uuid.uuid4().int
+        self.archived = False
+        self.tasklist = tasklist
+        self.user = user
+
+    def __repr__(self):
+        return "Task: \'%s\' with id %s" % (self.content,self.id)
+
+    #helper/instance methods
+    def complete(self):
+        self.completed = True
+
+    def uncomplete(self):
+        self.completed = False
+
+    def archive(self):
+        self.archive = True
+
+    def un_archive(self):
+        self.archive = False
+
+class Tasklist(db.Model):
+
+    __tablename__ = 'tasklists'
+    id = db.Column(db.BigInteger, primary_key=True)
+    date_created = db.Column(db.DateTime)
+    title = db.Column(db.String)
+    priority = db.Column(db.Integer)
+    archived = db.Column(db.Boolean)
+    # tasks = db.relationship("Task",backref="tasklists")
+
+
+
+    def __init__(self,title,priority=0):
+        self.tasks = {}
+        self.title = title
+        self.priority = priority
+        self.id = uuid.uuid4().int
+        self.date_created = datetime.utcnow()
+        self.archived = False
+        self.user = user
+
+    def __repr__(self):
+        return "Tasklist: \'%s\' with id %s" % (self.title,self.id)
+
+    def archive(self):
+        self.archive = True
+
+    def un_archive(self):
+        self.archive = False
+
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+    id = db.Column(db.BigInteger, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+    date_created = db.Column(db.DateTime)
+
+    # tasks = db.relationship("Task",backref="users")
+    # tasklists = db.relationship("Tasklist",backref="users")
+
+    def __init__(self,username,password):
+        self.id = uuid.uuid4().int
+        self.username = username
+        self.password = password
+        self.date_created = datetime.utcnow()
+
+    def __repr__(self):
+        return "User %s with id %s" % (self.username,self.id)
+
+    def is_authenticated(self):
+        """
+        Need to deal with three cases:
+        either the user exists and has the right password,
+        or the user exists but has the wrong password,
+        or the user doesn't exist and therefore the password doesn't matter
+        """
+        print 'authenticating'
+        user_query = User.query.filter_by(username=self.username).first()
+        if user_query is not None:
+            print 'username exists'
+            if user_query.password == self.password:
+                print 'password works'
+                return True
+        return False
+
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        'I am looking for this user'
+        user_query = User.query.filter_by(username=self.username).first()
+        return unicode(user_query.id)
+
+    def exists(self):
+        return (not User.query.filter_by(username=self.username) == None)
 
 
 if __name__ == '__main__':
