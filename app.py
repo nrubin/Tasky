a1a4ed1d2b161d4a7eb122ffa42c19d0b3dedd9e
Noam Rubin <noamrubin@live.com>
2012-08-25 03:28:32 -0400

    got logins to work. If there were tasks or tasklists in the db, Ithink those would populate correctly too. next steps are adding new-tasklist and new-task functionality to home.html.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import *
 import uuid
+import inspect
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TASKY_DB.db' #set db uri
@@ -12,28 +13,28 @@ app.secret_key = "\x8d\xd4\x9b\xef\x8fB\xa2\x02\xd9\x9a\xd5\xd4\x8eD\x1b'\xdf\n\
 db = SQLAlchemy(app) #build the associated db
 login_manager = LoginManager()
 login_manager.setup_app(app) #make sure logins work
-current_user = None
-
 @login_manager.user_loader
 def load_user(userid):
     """
     Given a user id, returns a User object associated with that id.
     Used by login manager for login stuff
     """
+    print "I am %s and my daddy is %s" % (whoami(),whosmydaddy())
     return User.query.filter_by(id=userid).first()
 
 @app.route('/')
 def root():
+    print "I am %s and my daddy is %s" % (whoami(),whosmydaddy())
     return render_template('login.html')
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    print "I am %s and my daddy is %s" % (whoami(),whosmydaddy())
     if request.method == 'POST': 
         user = User(request.form['username'],request.form['password']) #make a new user object (not pushed to DB yet)
         if user.is_authenticated(): #if this user object is legit and is in the DB and the password is right
             login_user(user) #we will mark the user logged in
-            print user
-            current_user = user
+            print 'current user is now %s' %(user)
             return redirect(url_for('home'))
         else:
             return 'User was not successfully authenticated'
@@ -42,12 +43,14 @@ def login():
 @app.route("/logout")
 @login_required
 def logout():
+    print "I am %s and my daddy is %s" % (whoami(),whosmydaddy())
     db.session.commit()
     logout_user()
     return 'goodbye'
 
 @app.route('/newaccount',methods=['POST'])
 def create_account():
+    print "I am %s and my daddy is %s" % (whoami(),whosmydaddy())
     new_user = User(request.form['username'],request.form['password'])
     if not new_user.exists():
         db.session.add(new_user)
@@ -61,19 +64,23 @@ def create_account():
 @app.route('/home')
 @login_required
 def home():
-    my_tasklists = Tasklist.query.filter_by(parent_user_id=current_user.get_id())
-    my_tasks = Tasks.query.filter_by(parent_user_id=current_user.get_id())
+    current_user_id = session['user_id']
+    print "I am %s and my daddy is %s" % (whoami(),whosmydaddy())
+    my_tasklists = Tasklist.query.filter_by(parent_user_id=current_user_id)
+    my_tasks = Task.query.filter_by(parent_user_id=current_user_id)
     task_data = {}
     for tasklist in my_tasklists:
         data[tasklist.id] = [task for task in my_tasks if task.parent_tasklist_id == tasklist.id]
-    render_template('home.html',task_data = task_data)
+    return render_template('home.html',task_data = task_data)
 
 @app.route('/createtasklist',methods='POST')
 @login_required
 def create_task_list():
+    print "I am %s and my daddy is %s" % (whoami(),whosmydaddy())
+    current_user_id = session['user_id']
     tasklist_title = request.form['tasklist_title']
     tasklist_title = request.get('priority',0)
-    current_user_id = current_user.get_id()
+    current_user_id = current_user_id
     new_tasklist = Tasklist(current_user_id,tasklist_title,priority)
     db.session.add(new_tasklist)
     return 'true'
@@ -81,17 +88,19 @@ def create_task_list():
 @app.route('/newtask',methods=['POST'])
 @login_required
 def create_task():
+    print "I am %s and my daddy is %s" % (whoami(),whosmydaddy())
+    current_user_id = session['user_id']
     task_content = request.form['task_content']
     deadline = request.get('deadline',None)
     priority = request.get('priority',0)
     parent_tasklist_id = request['parent_tasklist_id']
-    current_user_id = current_user.get_id()
     new_task = Task(parent_tasklist_id,current_user_id,task_content,deadline,priority)
     db.session.add(new_task)
     return 'true'
 
 @app.route('/updateTasks',methods=['GET'])
 def update_tasks():
+    print "I am %s and my daddy is %s" % (whoami(),whosmydaddy())
     return render_template('task.html',tasks=sorted(tasks, key=lambda task: task.date_created,reverse=True))
 
 
@@ -213,6 +222,12 @@ class User(db.Model):
 
     def exists(self):
         return (not (User.query.filter_by(username=self.username).first() == None))
+
+def whoami():
+    return inspect.stack()[1][3]
+
+def whosmydaddy():
+    return inspect.stack()[2][3]
 
 
 if __name__ == '__main__':
